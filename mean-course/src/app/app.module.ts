3472cb4725cae9aa32a191b8c5f5b447c81b84cd
dishubagga <dishubagga9111@gmail.com>
2020-02-24 13:03:23 +0100

    Adding Angular Material

--- mean-course/src/app/app.module.ts
+++ mean-course/src/app/app.module.ts
@@ -3,7 +3,10 @@ import { NgModule } from '@angular/core';
 import { PostCreateComponent } from './posts/post-create/post-create.component'
 import { PostListComponent } from './posts/post-list/post-list.component'
 import { AppComponent } from './app.component';
-import { FormsModule } from '@angular/forms'
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+
+import { MatInputModule, MatCardModule, MatButtonModule } from '@angular/material'
 @NgModule({
   declarations: [
     AppComponent,
@@ -12,7 +15,11 @@ import { FormsModule } from '@angular/forms'
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
