6683fdc6e583365fdf6bf59411c44b8a437960da
Dishu Bagga <dishubagga9111@gmail.com>
2020-02-26 11:49:02 +0100

    Adding functionalities in backend app.js
    adding functionalities like status and all

--- mean-course/backend/app.js
+++ mean-course/backend/app.js
@@ -1,13 +1,24 @@
 const express = require('express');
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('First middleware');
-    next();
-});
 
-app.use((req, res, next) => {
-    res.send('hello from expresss');
-})
+
+app.use('/api/posts' , (req, res, next) => {
+    const posts =   [
+        { id: 'fshhsh123',
+          title: 'First-Server-Side-Code',
+          content: 'this is coming from server'
+        },
+        { id: '12fshhsh123',
+          title: 'Second-Server-Side-Code',
+          content: 'this is coming from server'
+        }
+    ];
+    return res.status(200).json({ // for the success 
+        message: ' Posts fetched Succesfully',
+        posts: posts
+    });
+
+});
 
 module.exports = app;
