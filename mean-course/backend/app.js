b0d0deb44e7eca25ab558d1e4942401065d24ac8
Dishu Bagga <dishubagga9111@gmail.com>
2020-03-01 11:00:46 +0100

    Adding Posts statement on server side

--- mean-course/backend/app.js
+++ mean-course/backend/app.js
@@ -1,5 +1,21 @@
 const express = require('express');
 const app = express();
+const bodyParser = require(" body-parser ");
+
+app.use( bodyParser.json());
+app.use( bodyParser.urlencoded({ extended: false }));
+
+
+app.post("/api/posts", (req, res, next)=> {
+  const posts = req.body;
+  console.log("hello");
+  res.status(201).jason({
+    message: 'Post Added Sucessfully',
+    posts: posts
+  })
+})
+
+
 
 app.use((req, res, next)=> {
   res.setHeader("Access-Control-Allow-Origin", "*");
