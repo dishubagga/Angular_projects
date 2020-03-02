ede4c87f3aded85d25912c28f25003780674c376
Dishu Bagga <dishubagga9111@gmail.com>
2020-03-02 16:41:41 +0100

    Adding delete (not complete)

--- mean-course/src/app/posts/post.service.ts
+++ mean-course/src/app/posts/post.service.ts
@@ -2,6 +2,8 @@ import { Post } from './post.model';
 import { Subject} from 'rxjs';
 import { HttpClient } from  "@angular/common/http"
 import { Injectable } from '@angular/core';
+import { map } from 'rxjs/operators';
+
 
 @Injectable({providedIn: 'root'})
 export class PostsService {
@@ -10,9 +12,18 @@ export class PostsService {
   constructor(private http: HttpClient) {}
 
   getPosts(){
-    this.http.get< { message: string, posts: Post[]} >('http://localhost:3000/api/posts')
-    .subscribe((postData) => {
-      this.posts = postData.posts;
+    this.http.get< { message: string, posts: any} >('http://localhost:3000/api/posts')
+    .pipe(map(( postData )=> {
+      return postData.posts.map(post => {
+        return {
+          title: post.title,
+          content: post.content,
+          id: post._id
+        }
+      });
+    }))
+    .subscribe(( transformedPosts ) => {
+      this.posts = transformedPosts;
       this.postsUpdated.next([...this.posts]);
     }) 
     //return [...this.posts];
@@ -25,7 +36,7 @@ export class PostsService {
     const post: Post = {  title:title, content: content};
     this.http.post<{ message: string }>("http://localhost:3000/api/posts", post)
     .subscribe(responseData => {
-      console.log(responseData.message);
+    
       this.posts.push(post);
       this.postsUpdated.next([...this.posts]);
 
