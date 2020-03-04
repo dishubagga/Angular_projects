be3c67b60a3b23c22b8b5bfedd9cc8443f87752b
Dishu Bagga <dishubagga9111@gmail.com>
2020-03-04 14:58:58 +0100

    Adding Delete functionaliteds from post list to app.js

--- mean-course/src/app/posts/post.service.ts
+++ mean-course/src/app/posts/post.service.ts
@@ -22,13 +22,19 @@ export class PostsService {
     return this.postsUpdated.asObservable();
   }
   addPost(title: string, content: string){
-    const post: Post = {  title:title, content: content};
+    const post: Post = {  _id: null, title:title, content: content};
     this.http.post<{ message: string }>("http://localhost:3000/api/posts", post)
     .subscribe(responseData => {
       console.log(responseData.message);
       this.posts.push(post);
       this.postsUpdated.next([...this.posts]);
 
+    })
+  }
+  deletePost(postId: string) {
+    this.http.delete("http://localhost:3000/api/posts/" + postId)
+    .subscribe(()=> {
+      console.log(" Deleted ");
     })
   }
 
