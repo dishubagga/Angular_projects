6755252ecae3f80ba548df5bfccf1bb9da07561b
Dishu Bagga <dishubagga9111@gmail.com>
2020-03-12 17:42:01 +0100

    Building new mean app

--- mean-course/src/app/auth/auth-interceptor.ts
+++ mean-course/src/app/auth/auth-interceptor.ts
@@ -1,7 +1,9 @@
 import { HttpInterceptor, HttpRequest, HttpHandler } from '@angular/common/http';
-
+import { Injectable } from '@angular/core';
+@Injectable()
 export class AuthInterceptor implements HttpInterceptor{
     intercept( req: HttpRequest<any>, next: HttpHandler){
+        
         return next.handle(req);
     }
 }
