bc90d2a12d9f6c3e36cc0e34004a2cc3cc4e4b4b
Dishu Bagga <dishubagga9111@gmail.com>
2020-03-14 18:21:58 +0100

    handling errors

--- meantutorial/server.js
+++ meantutorial/server.js
@@ -26,8 +26,21 @@ app.post('/users', (req,res)=> {
     user.username   = req.body.username;
     user.password   = req.body.password;
     user.email      = req.body.email;
-    user.save(); 
-    res.send("user created");
+    if(req.body.username == null || req.body.username == '' || req.body.password == null || req.body.password == '' || req.body.email == null || req.body.email == '' ){
+        res.send("Ensure username, email, password is provided");
+    }
+    else {
+        user.save((err)=>{
+            if(err){
+                res.send("username or email already exsist");
+            }
+            else{
+                res.send("user created");
+            }
+        })
+    }
+   
+    
 })
 
 
