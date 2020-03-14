46501538b126840c9e0dfc2fd29d5b52a5f9cca7
Dishu Bagga <dishubagga9111@gmail.com>
2020-03-14 13:42:02 +0100

    connecting mongodb with nodejs

--- meantutorial/server.js
+++ meantutorial/server.js
@@ -2,8 +2,19 @@ var express = require("express");
 var app = express();
 var port = process.env.PORT || 8080;
 var morgan = require("morgan");
+var mongoose = require("mongoose");
 
 app.use(morgan('dev'));
+
+mongoose.connect('mongodb://localhost:27017/tutorial', (err)=> {
+    if(err){
+        console.log("Not connected to the database " + err);
+
+    }
+    else{
+        console.log("successfull connected to mongodb")
+    }
+});
 
 
 
