3632e8f5cc2190dbf2e8c91f0cdf1aceedb402c9
Dishu Bagga <dishubagga9111@gmail.com>
2020-03-16 19:14:26 +0100

    Sending http request to the server side code

--- meantutorial/public/app/controllers/userCtrl.js
+++ meantutorial/public/app/controllers/userCtrl.js
@@ -1,8 +1,10 @@
 angular.module('userControllers', [])
 
-.controller('regCtrl', function() {
+.controller('regCtrl', function($http) {
     this.regUser = function(regData){
         console.log("testing new button");
         console.log(this.regData);
+        $http.post('/api/users', this.regData);
     }
 })
+
