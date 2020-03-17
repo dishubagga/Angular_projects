9397efa55a941baac7168b1caadb5f87cf36f5c3
Dishu Bagga <dishubagga9111@gmail.com>
2020-03-17 12:17:48 +0100

    login working backend
    check in postman x urlnform encoded

--- meantutorial/app/routes/api.js
+++ meantutorial/app/routes/api.js
@@ -1,7 +1,7 @@
 var User    = require('../models/user');
 
 module.exports = (router)=>{
-        
+    // registrationRoute 
     router.post('/users', (req,res)=> {
         var user = new User();
         console.log(req.body.username);
@@ -25,5 +25,33 @@ module.exports = (router)=>{
        
         
     });
+
+    //login Route
+    router.post('/authenticate', function(req,res){
+        User.findOne({username: req.body.username }).select('email username password').exec(function(err, user){
+            if(err) throw  err;
+
+            if(!user){
+                console.log("if not user");
+                res.json({ success: false, message: 'Could not authenticate user'});
+            }
+            else if(user){
+                console.log("if valid password");
+                if(req.body.password){
+                    var validPassword = user.comparePassword(req.body.password);
+                }
+                else{
+                    res.json({ success: false, message: 'No password provided'});
+                }
+                if(!validPassword){
+                    res.json({ success: false, message: 'Could not authenticate password'});
+                }
+                else{
+                    res.json({ success: true, message: 'User authenticated!'});
+                }
+            }
+
+        })
+    })
     return router;
 }
