b0dfbe10ebd56c12e4f130539f058ce57da4e6d3
Dishu Bagga <dishubagga9111@gmail.com>
2020-03-19 18:01:42 +0100

    Adding front end part

--- epic-chat/src/app/app.component.ts
+++ epic-chat/src/app/app.component.ts
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'epic-chat';
+  title = 'Epic Chat';
+  message = '';
+  chats = [
+    'Hello',
+    'How are you',
+    'Im good, what abou you?'
+  ];
+  addChat() {
+    if(this.message.length === 0){
+      return
+    }else {
+      this.chats.push(this.message);
+      this.message = '';
+
+      window.setInterval(()=>{
+        const elem =  document.getElementById('scrolldiv'); //this is used for scroll down 
+        elem.scrollTop = elem.scrollHeight;
+      }, 500)
+      
+    }
+    
+  }
 }
